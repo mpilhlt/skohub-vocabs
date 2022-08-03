08d2e51e1c5ce4b8645faf1c5ae204fb98c2a165
Andreas Wagner <andreas.wagner@em.uni-frankfurt.de.fake>
2022-08-03 15:48:38 +0200

    React(-dom) to v18 (and 17).

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
+// FIXME: Enzyme tests cause problems with react18. Therefore, we provide react17 for testing and react18 for regular site use.
+// see https://darekkay.com/blog/react-18-enzyme/
 module.exports = {
   transform: {
     "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
   },
   moduleNameMapper: {
+    '^react-dom((\\/.*)?)$': 'react-dom17$1',
+    '^react((\\/.*)?)$': 'react17$1',
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
     "^gatsby-page-utils/(.*)$": `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
