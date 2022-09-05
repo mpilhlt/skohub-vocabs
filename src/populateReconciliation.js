711d2c3db71bae759b17cebe88680239a1dc52c7
Andreas Wagner <andreas.wagner@em.uni-frankfurt.de.fake>
2022-09-05 08:06:06 +0200

    Rename "tenant" to "account".

--- src/populateReconciliation.js
+++ src/populateReconciliation.js
@@ -15,24 +15,24 @@ async function collectData () {
 	var data = []
 	const files = glob.sync('data/**/*.ttl')
 	for (const f of files) {
-		const tenant = path.dirname(f).split(path.sep)[1]
-		console.log(`> Read and parse ${tenant}/${path.basename(f)} ...`)
-		if (!/[a-zA-Z0-9]/.test(tenant.slice(0,1))) {
-			console.log(`> Invalid data: tenant must start with a letter or a number. Instead, its value is: ${tenant}`)
+		const account = path.dirname(f).split(path.sep)[1]
+		console.log(`> Read and parse ${account}/${path.basename(f)} ...`)
+		if (!/[a-zA-Z0-9]/.test(account.slice(0,1))) {
+			console.log(`> Invalid data: account must start with a letter or a number. Instead, its value is: ${account}`)
 			continue
 		}
 		const ttlString = fs.readFileSync(f).toString()
-		const j = await buildJSON(ttlString, tenant)
+		const j = await buildJSON(ttlString, account)
 		if (!/[a-zA-Z0-9]/.test(j.vocab.slice(0,1))) {
 			console.log(`> Invalid data: vocab must start with a letter or a number. Instead, its value is: ${j.vocab}`)
 			continue
 		}
-		data.push({ tenant: j.tenant, vocab: j.vocab, entries: j.entries })
+		data.push({ account: j.account, vocab: j.vocab, entries: j.entries })
 	}
 	return data
 };
 
-async function buildJSON (ttlString, tenant) {
+async function buildJSON (ttlString, account) {
 	const doc = ttl2jsonld(ttlString)
 	const expanded = await jsonld.expand(doc)
 	const compacted = await jsonld.compact(expanded, context.jsonld)
@@ -55,13 +55,13 @@ async function buildJSON (ttlString, tenant) {
 			vocab = node.inScheme[0].id.substring(0, node.inScheme[0].id.lastIndexOf('/'))
 		}
 		node['vocab'] = vocab
-		node['tenant'] = tenant
+		node['account'] = account
 		node['@context'] = context.jsonld['@context']
 
 		entries = `${entries}{ "index" : { "_index" : "${esIndex}" } }\n`
 		entries = entries + JSON.stringify(node) + '\n'
 	})
-	return { tenant: tenant, vocab: vocab, entries: entries }
+	return { account: account, vocab: vocab, entries: entries }
 };
 
 var esClient
@@ -71,11 +71,11 @@ if (process.env.ES_USER && process.env.ES_PASS) {
 	esClient = new elasticsearch.Client({ node: `${process.env.ES_PROTO}://${process.env.ES_HOST}:${process.env.ES_PORT}` })
 }
 
-async function deleteData (tenant, vocab) {
+async function deleteData (account, vocab) {
 	const requestBody = esb.requestBodySearch()
 		.query(esb.boolQuery()
 			.must([ ...(vocab && [esb.termQuery('vocab', vocab)]),
-					...(tenant && [esb.termQuery('tenant', tenant)])
+					...(account && [esb.termQuery('account', account)])
 				])
 		)
 	return esClient.deleteByQuery({
@@ -95,28 +95,28 @@ async function sendData (entries) {
 async function main() {
 	const data = await collectData()
 	data.forEach(async v => {
-		await deleteData(v.tenant, v.vocab)
+		await deleteData(v.account, v.vocab)
 		.then(response => {
 			if (response.statusCode !== 200) {
-				console.log(`> Warning: Delete ${v.tenant}/${v.vocab} status != 200. Better check response:\n`, response)
+				console.log(`> Warning: Delete ${v.account}/${v.vocab} status != 200. Better check response:\n`, response)
 			} else {
-				console.log(`> ${v.tenant}/${v.vocab}: Successfully deleted ${response.body.deleted} documents from ES index.`)
+				console.log(`> ${v.account}/${v.vocab}: Successfully deleted ${response.body.deleted} documents from ES index.`)
 			}
 		})
 		.catch(error => {
-			console.error(`Failed populating ${esIndex} index of ES server when trying to delete ${v.tenant}/${v.vocab}. Abort!`, error)
+			console.error(`Failed populating ${esIndex} index of ES server when trying to delete ${v.account}/${v.vocab}. Abort!`, error)
 		})
 
 		await sendData(v.entries)
 		.then(response => {
 			if (response.statusCode !== 200) {
-				console.log(`> Warning: SendData ${v.tenant}/${v.vocab} status != 200. Better check response:\n`, response)
+				console.log(`> Warning: SendData ${v.account}/${v.vocab} status != 200. Better check response:\n`, response)
 			} else {
-				console.log(`> ${v.tenant}/${v.vocab}: Successfully sent ${response.body.items.length} documents to ES index.`)
+				console.log(`> ${v.account}/${v.vocab}: Successfully sent ${response.body.items.length} documents to ES index.`)
 			}
 		})
 		.catch(error => {
-			console.error(`Failed populating ${esIndex} index of ES server with ${v.tenant}/${v.vocab}. Abort!`, error)
+			console.error(`Failed populating ${esIndex} index of ES server with ${v.account}/${v.vocab}. Abort!`, error)
 		})
 	})
 }
